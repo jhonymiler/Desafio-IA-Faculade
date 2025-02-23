896c9eed70178735046e7ffdc44e42c294259fad
Jhony Miler <jonatasmiler@gmail.com>
2025-02-23 12:26:52 -0300

    removendo pasta api

--- app.py
+++ app.py
@@ -13,5 +13,6 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ""      # Força o uso apenas da CPU
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"       # Reduz os avisos de nível INFO e WARNING
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"  # Corrige erro de incompatibilidade
 
-if __name__ == '__main__':
-   app.run(debug=False, port=10000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render define a porta automaticamente
+    app.run(host="0.0.0.0", port=port, debug=False)
